3f2e80a82b38826639aa22b1e79119c2bb895904
Sven <sven.boekhoven@e-sven.nl>
2015-02-08 11:48:40 +0100

    small changes

--- hourrecord.R
+++ hourrecord.R
@@ -1,6 +1,5 @@
 library("ggplot2")
 library("dplyr")
-library("animation")
 library("grid")
 
 allhourdata <- read.csv("hour record.csv", sep=";", header=TRUE, dec=",")
@@ -21,7 +20,7 @@ allhourdata <- allhourdata %>% mutate(Actual.laptimes.cummulative = cumsum(Actua
         mutate(laptime.to.slow = ifelse(Actual.laptimes < Required.laptimes.for.record, 0, Actual.laptimes))
 
 begin_ani <- 150
-end_ani <- 206
+end_ani <- nrow(allhourdata)
 
 for (i in begin_ani:end_ani){
         var <- allhourdata[i,]
@@ -49,25 +48,25 @@ for (i in begin_ani:end_ani){
                 theme(axis.title.y=element_text(size=11,colour="#535353",face="bold",vjust=1.5)) +
                 theme(axis.title.x=element_text(size=11,colour="#535353",face="bold",vjust=-.5))
         
-        gg <- gg + theme(plot.margin = unit(c(1.5, 1.5, 1, 1), "cm")) +
-                annotate("text",x=as.numeric(var[,1])+2,y=as.numeric(var[,3]),label=round(as.numeric(var[,3]),digits=3),colour="#000099", hjust = 0)
+        gg <- gg + theme(plot.margin = unit(c(1.5, 1.5, 1, 1), "cm"))
         
         ## Laptimes
-        gg <- gg + geom_point(data=hourdata, aes(x=Id, y=Actual.laptimes), colour="#000099", size=3)
-
-#         ## Average Laptimes
-#         gg <- gg + geom_line(data=hourdata, aes(x=Id, y=Average.actual.laptimes), colour="#909090", size=1/2)
-# 
+        gg <- gg + geom_point(data=hourdata, aes(x=Id, y=Actual.laptimes), colour="#000099", size=3) +
+                annotate("text",x=as.numeric(var[,1])+2,y=as.numeric(var[,3]),label=round(as.numeric(var[,3]),digits=3),colour="#000099", hjust = 0)
+        
+        #         ## Average Laptimes
+        #         gg <- gg + geom_line(data=hourdata, aes(x=Id, y=Average.actual.laptimes), colour="#909090", size=1/2)
+        # 
         ## Required Laptimes
         gg <- gg + geom_line(data=hline_req, aes(x=Id, y=req), colour=avg_color, size=1, alpha=1/2) +
-        annotate("text",x=206+2,y=as.numeric(var[,11]),label=round(var[,12],digits=3),colour=avg_color, hjust = 0, alpha=1/2) +
-        annotate("text",x=0,y=as.numeric(var[,11]),label=round(var[,11],digits=3),colour=avg_color, hjust = 1, alpha=1/2) +
-        annotate("text",x=206,y=as.numeric(var[,11])+0.075,label="Required for record",colour=avg_color, hjust = 1, alpha=1/2)
+                annotate("text",x=206+2,y=as.numeric(var[,11]),label=round(var[,12],digits=3),colour=avg_color, hjust = 0, alpha=1/2) +
+                annotate("text",x=0,y=as.numeric(var[,11]),label=round(var[,11],digits=3),colour=avg_color, hjust = 1, alpha=1/2) +
+                annotate("text",x=206,y=as.numeric(var[,11])+0.075,label="Required for record",colour=avg_color, hjust = 1, alpha=1/2)
         
         ## Current Record line
         gg <- gg + geom_line(data=hline_rec, aes(x=Id, y=rec),colour="#909090", size=1, linetype=2) +
                 annotate("text",x=206+2,y=as.numeric(var[,6]),label="51.852",colour="#909090", hjust = 0) +
-        annotate("text",x=0,y=as.numeric(var[,6]),label=round(var[,6],digits=3),colour="#909090", hjust = 1)
+                annotate("text",x=0,y=as.numeric(var[,6]),label=round(var[,6],digits=3),colour="#909090", hjust = 1)
         
         ## Average speed/laptime line
         gg <- gg + geom_line(data=hline_avg, aes(x=Id, y=avg),colour=avg_color, size=1, alpha=1/2) +
@@ -76,7 +75,7 @@ for (i in begin_ani:end_ani){
                 annotate("text",x=1,y=as.numeric(var[,5])+0.075,label="Average",colour=avg_color, hjust = 0, alpha=1/2)
         
         ## Required laptimes text
-#        gg <- gg + annotate("text",x=152,y=16.1,label=paste("Required laptime for record:", round(as.numeric(var[,11]), digits=3)),colour="#3C3C3C", hjust = 0)
+        #        gg <- gg + annotate("text",x=152,y=16.1,label=paste("Required laptime for record:", round(as.numeric(var[,11]), digits=3)),colour="#3C3C3C", hjust = 0)
         
         gg <- gg + scale_x_continuous(minor_breaks=0,breaks=seq(0,200,50),limits=c(-5,216)) +
                 scale_y_continuous(minor_breaks=0,breaks=seq(16, 18.5, 0.5)) + #,limits=c(16, 18.5)) +
